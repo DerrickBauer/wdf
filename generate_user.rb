798da8a49b065f4e732bdb5ab29e2ec6b0ddac09
Derrick Bauer <derrickbauer@gmail.com>
2015-05-26 20:37:43 -0400

    removed unused

--- generate_user.rb
+++
@@ -1,6 +0,0 @@
-require 'bcrypt'
-puts "Enter Desired Password:"
-password = gets
-salt = BCrypt::Engine.generate_salt
-encrypted_password = BCrypt::Engine.hash_secret(password, salt)
-puts "Encrypted Password:#{encrypted_password}"
